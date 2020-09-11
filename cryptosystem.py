b0ce7a96ab7d0aaea5c1014a8b648bd911fe9fdc
GaudiestTooth17 <alex.r.anthon@gmail.com>
2020-09-11 13:55:14 -0600

    encrypting works!

--- cryptosystem.py
+++ cryptosystem.py
@@ -1,13 +1,24 @@
 import numpy as np
 import math
 import sys
+from typing import List
+import itertools
 
 
 def main():
-    key = sys.argv[1]
-    plain_text_file_name = sys.argv[2]
+    mode = sys.argv[1]
+    key = sys.argv[2]
+    file_name = sys.argv[3]
+    if mode == 'e':
+        encrypt(file_name, key)
+    elif mode == 'd':
+        decrypt(file_name, key)
+    else:
+        print(f'Unrecognized mode {mode}.')
 
-    with open(plain_text_file_name, 'r') as f:
+
+def encrypt(file_name: str, key: str) -> None:
+    with open(file_name, 'r') as f:
         lines = f.readlines()
     plain_text = ''
     for line in lines:
@@ -16,10 +27,50 @@ def main():
     text_matrix = text_to_matrix(plain_text)
     key_matrix = key_to_matrix(key, text_matrix.shape[0])
     encrypted_matrix = np.matmul(key_matrix, text_matrix)
+    # print(matrix_to_plain_text(encrypted_matrix))
     cipher_text = matrix_to_text(encrypted_matrix)
 
-    with open('encrypted_'+plain_text_file_name, 'w') as f:
+    with open('encrypted_'+file_name, 'w') as f:
         f.write(cipher_text)
+
+
+def decrypt(file_name: str, key: str) -> None:
+    with open(file_name, 'r') as f:
+        lines = f.readlines()
+    for i in range(len(lines)):
+        while lines[i][-1] == '\n':
+            lines[i] = lines[i][:-1]
+    entries = ''.join(lines).split(' ')
+
+    encrypted_matrix = entries_to_matrix(entries)
+    key_matrix = np.linalg.inv(key_to_matrix(key, encrypted_matrix.shape[0]))
+    decrypted_matrix = np.matmul(key_matrix, encrypted_matrix)
+    plain_text = matrix_to_plain_text(decrypted_matrix)
+
+    with open('decrypted_'+file_name, 'w') as f:
+        f.write(plain_text)
+
+
+def entries_to_matrix(entries: List[str]) -> np.ndarray:
+    n = int(math.sqrt(len(entries)))
+    matrix = np.zeros((n, n), dtype=np.uint32)
+
+    index = 0
+    for i in range(n):
+        for j in range(n):
+            matrix[i][j] = int(entries[index], 16)
+            index += 1
+
+    return matrix
+
+
+def matrix_to_plain_text(matrix: np.ndarray) -> str:
+    text = ''
+    for i in range(matrix.shape[0]):
+        for j in range(matrix.shape[0]):
+            print(matrix[i][j])
+            text += chr(matrix[i][j])
+    return text
 
 
 def key_to_matrix(key: str, n: int) -> np.ndarray:
@@ -27,25 +78,18 @@ def key_to_matrix(key: str, n: int) -> np.ndarray:
     Creates an invertible matrix from the provided string.
     """
     key_matrix = np.identity(n, dtype=np.uint32)
-    # swap two rows of the matrix
-    row1, row2 = substring_to_numbers(key[:2], n)
-    key_matrix[row1], key_matrix[row2] = key_matrix[row2], key_matrix[row1]
 
-    # multiply row
-    row, factor = substring_to_numbers(key[2:4], n)
-    key_matrix[row] *= factor
+    new_key = ''
+    for i in range(n):
+        new_key += key[i % len(key)]
 
-    # swap rows
-    row1, row2 = substring_to_numbers(key[4:6], n)
-    key_matrix[row1], key_matrix[row2] = key_matrix[row2], key_matrix[row1]
+    for i, key_element in enumerate(new_key):
+        key_matrix[i] *= ord(key_element) % 32
 
-    # add two rows
-    row1, row2 = substring_to_numbers(key[6:8], n)
-    key_matrix[row1] += key_matrix[row2]
-
-    # multiply row
-    row, factor = substring_to_numbers(key[8:10], n)
-    key_matrix[row] *= factor
+    for i in range(0, len(new_key)-1, 2):
+        print(new_key[i:i+2])
+        row1, row2, x = substring_to_numbers(new_key[i:i+2], n)
+        key_matrix[row1] += (x % 32) * key_matrix[row2]
 
     return key_matrix
 
@@ -55,9 +99,11 @@ def substring_to_numbers(sub_str: str, n: int):
     Substring should contain two characters
     :return: Two numbers in range(n)
     """
-    num1 = (3 * ord(sub_str[0])) % n
+    num1 = ord(sub_str[0]) % n
     num2 = (5 * ord(sub_str[1])) % n
-    return num1, num2
+    num3 = (ord(sub_str[0]) + ord(sub_str[1])) % n
+    print((num1, num2, num3))
+    return num1, num2, num3
 
 
 def text_to_matrix(plain_text: str) -> np.ndarray:
